c605e5cc06eca0fe4eefce96496fd8f5b814083b
VAA <VasilyevAA>
2020-06-04 17:29:48 +0300

    fix a little mistakes

--- tests/test_division.py
+++ tests/test_division.py
@@ -12,10 +12,12 @@ from tests.utils import STATUS_CODES
         ('5', '1.25', '4'),
         ('15', '-2.500000', '-6'),
 
-        ('0.1', '8', '-0.05'),
-        ('10.4', '2.', '5.2'),
+        ('0.1', '4', '0.025'),
+        ('10.4', '2.0', '5.2'),
         ('-0.1', '10', '-0.01'),
-        ('-1.6', '4', '-0.04'),
+        ('-1.6', '4', '-0.4'),
+        ('-5', '2', '-2.5'),
+        ('100', '3', '33.33333333333333333333333333'),
 
         ('0.000001', '-0.000001', '-1'),
         ('0.000001', '9'*20, '10.0000000000000000001E-27'),
@@ -24,7 +26,7 @@ from tests.utils import STATUS_CODES
         ('0', '-0.222', '0'),
     ]
 )
-def test_positive_division_by_number_with_max_count_of_decimals(division_request, arg1, arg2, expected_result):
+def test_positive_division(division_request, arg1, arg2, expected_result):
     code, data = division_request({'arg1': arg1, 'arg2': arg2})
     assert code == STATUS_CODES.ok
     assert data['result'] == expected_result
--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -11,8 +11,8 @@ from tests.utils import STATUS_CODES
     ('-4', '-5.6', '22.4'),
     ('-0.000001', '0.000001', '-1E-12'),
     ('1231231', '2', '2462462'),
-    ('20', '2.5', '50.0'),
-    ('9'*20, '9'*20+'.01', '9.999999999999999999801000000E+39')
+    ('20', '2.5', '50'),
+    ('9'*20, '9'*20+'.01', '9.999999999999999999801E+39')
 ])
 @pytest.mark.parametrize('reverse_args', [True, False])
 def test_positive_multiplication(multiplication_request, arg1, arg2, expected_result, reverse_args):
