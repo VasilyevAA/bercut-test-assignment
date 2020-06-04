fbf5aacd9271730aba834fcea2961b5b968674b7
VAA <VasilyevAA>
2020-06-04 16:10:33 +0300

    test_additional complete mb need more review for design

--- tests/test_additional.py
+++ tests/test_additional.py
@@ -1,9 +1,27 @@
+import pytest
+
+from tests.utils import STATUS_CODES
 
 
-
-def test_positive_additional(additional_request):
-    code, data = additional_request({'arg1': '111', 'arg2': '231.12'})
-
-def test_positive_additional_very_big_numbers(test_client):
-    data = test_client.post("/v1/add", json={'arg1': '1234567890', 'arg2': '231.12'})
-    print('keks')
+@pytest.mark.parametrize(
+    'arg1, arg2, expected_result',
+    [
+        ('1', '2', '3'),
+        ('-3', '4', '1'),
+        ('-6', '5', '-1'),
+        ('0.1', '1', '1.1'),
+        ('-1.8', '1.1', '-0.7'),
+        ('-1.8', '-10', '-11.8'),
+        ('1.8', '1.2', '3'),
+        ('0', '1.2', '1.2'),
+        ('-0.000000', '11', '11'),
+        ('9'*27, '9'*27, '1999999999999999999999999998'),
+    ]
+)
+@pytest.mark.parametrize('reverse_args', [True, False])
+def test_positive_additional(additional_request, arg1, arg2, expected_result, reverse_args):
+    if reverse_args:
+        arg1, arg2 = arg2, arg1
+    code, data = additional_request({'arg1': arg1, 'arg2': arg2})
+    assert code == STATUS_CODES.ok
+    assert data['result'] == expected_result
